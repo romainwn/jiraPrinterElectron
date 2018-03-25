bd191867e8fb61912887ee72ceee0f204658ffd5
statox <adrienfabre@protonmail.com>
2018-03-25 20:39:13 +0200

    Replace  by XMLHttpRequest to avoid issues with system proxy settings

--- scripts/resources/issue.js
+++ scripts/resources/issue.js
@@ -1,15 +1,36 @@
+/*
+ * We use XMLHttpRequest() instead of $http so we don't have to
+ * deal manually with the user proxy settings
+ */
 angular.module('app').factory("Issue", function($http) {
     return {
-        get: function(issueID, host, port, username, password) {
-            return $http({
-                method: 'GET',
-                url: 'http://' + host + ":" + port + '/rest/api/latest/issue/' + issueID,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Basic ' + btoa(username + ':' + password),
-                    'Access-Control-Allow-Origin': '*'
-                },
-                timeout: 2 * 60 * 1000
+        get: function (issueID, host, port, username, password) {
+            return new Promise(function (resolve, reject) {
+                var xhr = new XMLHttpRequest();
+
+                xhr.open("GET", 'http://' + host + ":" + port + '/rest/api/latest/issue/' + issueID);
+                xhr.setRequestHeader("Authorization", 'Basic ' + btoa(username + ':' + password));
+
+                xhr.onload = function () {
+                    if (this.status >= 200 && this.status < 300) {
+                        resolve({
+                            status: this.status,
+                            data: angular.fromJson(xhr.response)
+                        });
+                    } else {
+                        reject({
+                            status: this.status,
+                            statusText: xhr.statusText
+                        });
+                    }
+                };
+                xhr.onerror = function () {
+                    reject({
+                        status: this.status,
+                        statusText: xhr.statusText
+                    });
+                };
+                xhr.send();
             });
         }
     }
